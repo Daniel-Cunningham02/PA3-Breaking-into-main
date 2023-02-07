1e6faa454092b69493b51a1c7a4498986a072f2d
Daniel Cunningham <danrcunn@ut.utm.edu>
2023-02-07 14:06:07 -0600

    Created new main along with MessageBox. There are still errors that need to be fixed. Need to create Label for the new main class as well.

--- PA3_Breaking_into_main/App.xaml.cs
+++ PA3_Breaking_into_main/App.xaml.cs
@@ -2,9 +2,14 @@
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
+using System.Diagnostics;
 using System.Linq;
+using System.Reflection.Emit;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Controls;
+using System.Xaml;
+using System.Xaml.Permissions;
 
 namespace PA3_Breaking_into_main
 {
--- PA3_Breaking_into_main/MainWindow.xaml.cs
+++ PA3_Breaking_into_main/MainWindow.xaml.cs
@@ -8,6 +8,7 @@ using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
+using System.Windows.Markup;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
@@ -22,7 +23,17 @@ namespace PA3_Breaking_into_main
     {
         public MainWindow()
         {
+
             InitializeComponent();
+            
+        }
+        static void Main(string[] args)
+        {
+            PA3_Breaking_into_main.App app = new PA3_Breaking_into_main.App();
+            app.InitializeComponent();
+            app.Run();
+
+            MessageBox.Show("Hello from Main!");
         }
     }
 }
